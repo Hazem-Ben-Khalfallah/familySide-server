c1841d8f45f7c531725b98a97c545f5953970395
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2017-01-29 21:33:32 +0100

    first commit

---
+++ src/main/java/com/blacknebula/familySide/FamilySideServerApplication.java
@@ -0,0 +1,12 @@
+package com.blacknebula.familySide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilySideServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilySideServerApplication.class, args);
+	}
+}
