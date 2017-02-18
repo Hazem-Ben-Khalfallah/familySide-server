ad6f66f49263a33f1fb87ad98cdbc94ca8ae2af5
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2017-02-18 18:07:01 +0100

    integrate openshift config

--- src/main/java/com/blacknebula/familySide/repository/MongoConfiguration.java
+++ src/main/java/com/blacknebula/familySide/repository/MongoConfiguration.java
@@ -1,17 +1,13 @@
 package com.blacknebula.familySide.repository;
 
-import com.blacknebula.familySide.model.UserCoordinates;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import java.util.Date;
 
 /**
  * @author Sebastien Deleuze
