23582239d4d7c556fc7f91c6d8ccd836430eca57
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2017-02-20 22:44:23 +0100

    make unit test use StepVerifier

---
+++ src/test/java/com/blacknebula/familySide/ApplicationTest.java
@@ -0,0 +1,44 @@
+package com.blacknebula.familySide;
+
+import com.mongodb.BasicDBObject;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.function.Consumer;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * @author hazem
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Before
+    public void setUp() throws Exception {
+        clearMongo();
+        initMocks(this);
+    }
+
+    /**
+     * Remove all data from collections (expect system collections). Faster than
+     * a simple dropDatabase.
+     */
+    private void clearMongo() throws Exception {
+        mongoTemplate.getDb().listCollectionNames()
+                .forEach((Consumer<? super String>) name -> {
+                    if (!name.startsWith("system.")) {
+                        mongoTemplate.getDb().getCollection(name)
+                                .deleteMany(new BasicDBObject());
+                    }
+                });
+    }
+}
--- src/test/java/com/blacknebula/familySide/authentication/UserServiceTest.java
+++ src/test/java/com/blacknebula/familySide/authentication/UserServiceTest.java
@@ -1,56 +1,24 @@
 package com.blacknebula.familySide.authentication;
 
+import com.blacknebula.familySide.ApplicationTest;
 import com.blacknebula.familySide.common.exception.CustomException;
-import com.mongodb.BasicDBObject;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Mono;
-
-import java.util.function.Consumer;
-
-import static org.mockito.MockitoAnnotations.initMocks;
+import reactor.test.StepVerifier;
 
 /**
  * @author hazem
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserServiceTest {
+
+public class UserServiceTest extends ApplicationTest {
 
     @Autowired
     private UserService userService;
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Before
-    public void setUp() throws Exception {
-        clearMongo();
-        initMocks(this);
-    }
-
-    /**
-     * Remove all data from collections (expect system collections). Faster than
-     * a simple dropDatabase.
-     */
-    private void clearMongo() throws Exception {
-        mongoTemplate.getDb().listCollectionNames()
-                .forEach((Consumer<? super String>) name -> {
-                    if (!name.startsWith("system.")) {
-                        mongoTemplate.getDb().getCollection(name)
-                                .deleteMany(new BasicDBObject());
-                    }
-                });
-    }
 
     /**
      * @verifies throw an exception if username is empty or null
@@ -58,19 +26,19 @@ public class UserServiceTest {
      */
     @Test
     public void signIn_shouldThrowAnExceptionIfUsernameIsEmptyOrNull() throws Exception {
-        try {
-            // when
-            userService.signIn(Mono.just(UserDto.builder()
-                    .email("Leo@mail.com")//
-                    .password("password")//
-                    .build()))
-                    .block();
-            Assert.fail("shouldThrowAnExceptionIfUsernameIsEmptyOrNull");
-        } catch (CustomException e) {
-            // then
-            Assertions.assertThat(e.getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
-            Assertions.assertThat(e.getMessage()).isEqualToIgnoringCase("username should not be empty nor null");
-        }
+        StepVerifier.create(
+                // when
+                userService.signIn(Mono.just(UserDto.builder()
+                        .email("Leo@mail.com")//
+                        .password("password")//
+                        .build())))
+                // then
+                .consumeErrorWith(throwable -> {
+                    Assertions.assertThat(throwable).isInstanceOf(CustomException.class);
+                    Assertions.assertThat(((CustomException) throwable).getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
+                    Assertions.assertThat(throwable.getMessage()).isEqualToIgnoringCase("username should not be empty nor null");
+                })
+                .verify();
     }
 
     /**
@@ -79,19 +47,19 @@ public class UserServiceTest {
      */
     @Test
     public void signIn_shouldThrowAnExceptionIfEmailIsEmptyOrNull() throws Exception {
-        try {
-            // when
-            userService.signIn(Mono.just(UserDto.builder()
-                    .username("Leo")//
-                    .password("password")//
-                    .build()))
-                    .block();
-            Assert.fail("shouldThrowAnExceptionIfEmailIsEmptyOrNull");
-        } catch (CustomException e) {
-            // then
-            Assertions.assertThat(e.getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
-            Assertions.assertThat(e.getMessage()).isEqualToIgnoringCase("email should not be empty nor null");
-        }
+        StepVerifier.create(
+                // when
+                userService.signIn(Mono.just(UserDto.builder()
+                        .username("Leo")//
+                        .password("password")//
+                        .build())))
+                // then
+                .consumeErrorWith(throwable -> {
+                    Assertions.assertThat(throwable).isInstanceOf(CustomException.class);
+                    Assertions.assertThat(((CustomException) throwable).getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
+                    Assertions.assertThat(throwable.getMessage()).isEqualToIgnoringCase("email should not be empty nor null");
+                })
+                .verify();
     }
 
     /**
@@ -100,19 +68,19 @@ public class UserServiceTest {
      */
     @Test
     public void signIn_shouldThrowAnExceptionIfPasswordIsEmptyOrNull() throws Exception {
-        try {
-            // when
-            userService.signIn(Mono.just(UserDto.builder()
-                    .username("Leo")//
-                    .email("Leo@mail.com")//
-                    .build()))
-                    .block();
-            Assert.fail("shouldThrowAnExceptionIfPasswordIsEmptyOrNull");
-        } catch (CustomException e) {
-            // then
-            Assertions.assertThat(e.getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
-            Assertions.assertThat(e.getMessage()).isEqualToIgnoringCase("password should not be empty nor null");
-        }
+        StepVerifier.create(
+                // when
+                userService.signIn(Mono.just(UserDto.builder()
+                        .username("Leo")//
+                        .email("Leo@mail.com")//
+                        .build())))
+                // then
+                .consumeErrorWith(throwable -> {
+                    Assertions.assertThat(throwable).isInstanceOf(CustomException.class);
+                    Assertions.assertThat(((CustomException) throwable).getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
+                    Assertions.assertThat(throwable.getMessage()).isEqualToIgnoringCase("password should not be empty nor null");
+                })
+                .verify();
     }
 
     /**
@@ -123,28 +91,27 @@ public class UserServiceTest {
     public void signIn_shouldThrowAnExceptionIfUsernameAlreadyExists() throws Exception {
         // given
         final String username = "Leo";
-        userRepository.save(UserEntity.builder()
-                .username(username)//
-                .email("Layth@mail.com")//
-                .password("password")//
-                .build())
-                .block();
 
-        try {
-            // when
-            userService.signIn(Mono.just(UserDto.builder()
-                    .username(username)//
-                    .password("password")
-                    .email("Leo@mail.com")//
-                    .build()))
-                    .block();
-            Assert.fail("shouldThrowAnExceptionIfUsernameAlreadyExists");
-        } catch (CustomException e) {
-            // then
-            Assertions.assertThat(e.getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
-            Assertions.assertThat(e.getMessage()).isEqualToIgnoringCase("username already used");
-        }
-
+        StepVerifier.create(
+                // given
+                userRepository.save(UserEntity.builder()
+                        .username(username)//
+                        .email("Layth@mail.com")//
+                        .password("password")//
+                        .build())
+                        // when
+                        .then(userService.signIn(Mono.just(UserDto.builder()
+                                .username(username)//
+                                .password("password")
+                                .email("Leo@mail.com")//
+                                .build()))))
+                // then
+                .consumeErrorWith(throwable -> {
+                    Assertions.assertThat(throwable).isInstanceOf(CustomException.class);
+                    Assertions.assertThat(((CustomException) throwable).getCustomErrorCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
+                    Assertions.assertThat(throwable.getMessage()).isEqualToIgnoringCase("username already used");
+                })
+                .verify();
     }
 
     /**
@@ -155,18 +122,20 @@ public class UserServiceTest {
     public void signIn_shouldCreateNewUserInDatabase() throws Exception {
         final String username = "Leo";
 
-        // when
-        userService.signIn(Mono.just(UserDto.builder()
-                .username(username)//
-                .password("password")
-                .email("Leo@mail.com")//
-                .build()))
-                .block();
-
-        // then
-        final UserEntity userEntity = userRepository.findOne(username)
-                .block();
-        Assertions.assertThat(userEntity).isNotNull();
-
+        StepVerifier.create(
+                // when
+                userService.signIn(Mono.just(UserDto.builder()
+                        .username("Leo")//
+                        .password("password")//
+                        .email("Leo@mail.com")//
+                        .build()))
+                        .then(userRepository.findOne(username)))
+                // then
+                .consumeNextWith(userEntity -> {
+                    Assertions.assertThat(userEntity).isNotNull();
+                    Assertions.assertThat(userEntity.getUsername()).isEqualTo(username);
+                })
+                .expectComplete()
+                .verify();
     }
 }
