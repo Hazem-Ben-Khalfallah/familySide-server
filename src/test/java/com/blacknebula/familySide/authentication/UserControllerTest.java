37f4a710b1d2e655e889ba88fa8009e5dbd215b6
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2017-02-21 22:59:22 +0100

    add teststo controllers

--- src/test/java/com/blacknebula/familySide/authentication/UserControllerTest.java
+++ src/test/java/com/blacknebula/familySide/authentication/UserControllerTest.java
@@ -1,21 +1,57 @@
 package com.blacknebula.familySide.authentication;
 
-import org.junit.Assert;
-import org.junit.Ignore;
+import com.blacknebula.familySide.ApplicationTest;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 /**
  * @author hazem
  */
-public class UserControllerTest {
+public class UserControllerTest extends ApplicationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+
+    private WebTestClient client;
+
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        this.client = WebTestClient.bindToController(new UserController(userService)).build();
+    }
+
+
     /**
      * @verifies return 200 status
      * @see UserController#connect(reactor.core.publisher.Mono)
      */
     @Test
-    @Ignore
     public void connect_shouldReturn200Status() throws Exception {
-        Assert.fail("Not yet implemented");
+        final String username = "Leo";
+        // when
+        this.client.post()
+                .uri("/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange(Mono.just(UserDto.builder()
+                        .username(username)
+                        .email("Leo@mail.com")
+                        .password("password")
+                        .build()), UserDto.class)
+                .expectStatus()
+                .isEqualTo(HttpStatus.OK.value());
+
+        //then
+        StepVerifier.create(userRepository.findOne(username))
+                .expectNextCount(1);
     }
 
     /**
@@ -23,9 +59,14 @@ public class UserControllerTest {
      * @see UserController#connect(reactor.core.publisher.Mono)
      */
     @Test
-    @Ignore
     public void connect_shouldReturnValidErrorStatusIfAnExceptionHasBeenThrown() throws Exception {
-        //TODO auto-generated
-        Assert.fail("Not yet implemented");
+        this.client.post()
+                .uri("/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange(Mono.just(UserDto.builder()
+                        .email("Leo@mail.com")
+                        .password("password")
+                        .build()), UserDto.class)
+                .expectStatus().isBadRequest();
     }
 }
